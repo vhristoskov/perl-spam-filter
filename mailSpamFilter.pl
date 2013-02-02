7aace926fb2cba8c898597750d4fb1eabd359503
Victor Hristoskov <victor.hristoskov@mail.bg>
2013-02-02 16:03:17 +0200

    Added configuration
    We'll read the mail configurations from the config.ini file

--- mailSpamFilter.pl
+++ mailSpamFilter.pl
@@ -1,14 +1,17 @@
 #!/usr/bin/perl -w
 use strict;
+use warnings;
 
 use Mail::POP3Client;
 use Email::MIME;
-use DBI;
+use Config::IniFiles;
 
-my $username = 'perl.spam.test@gmail.com';
-my $password = 'perl.test';
-my $mailhost = 'pop.gmail.com';
-my $port = '995';
+#use DBI;
+
+#my $username = 'perl.spam.test@gmail.com';
+#my $password = 'perl.test';
+#my $mailhost = 'pop.gmail.com';
+#my $port = '995';
 
 my @emails;
 
@@ -51,39 +54,46 @@ sub getEmails{
     return @emails;
 }
 
-sub dbManipulation{
-    my $driver = 'mysql';
-    my $database = 'PERLTEST';
-    my $dbSourceName = "DBI:$driver:$database";
-    my $userId = 'perl.test';
-    my $password = 'perl.test';
-    
-    my $dbh = DBI->connect($dbSourceName, $userId, $password)
-                or die "Couldn't connect to database: ". DBI->errstr;
-    
-    my $sth = $dbh->prepare_cached('SELECT * FROM People')
-                    or die "Couldn't prepare statement: ". $dbh->errstr;
-                    
-    $sth->execute
-            or die "Couldn't execute statement: ". $sth->errstr;
-    my @personData;
-    
-    while( @personData = $sth->fetchrow_array()){
-        print "\t$personData[0]: $personData[1] $personData[2] $personData[3]\n"; 
-    }
-    
-    $sth->finish;
-    print "\n";
-    
-    $dbh->disconnect; 
-}
+#sub dbManipulation{
+#    my $driver = 'mysql';
+#    my $database = 'PERLTEST';
+#    my $dbSourceName = "DBI:$driver:$database";
+#    my $userId = 'perl.test';
+#    my $password = 'perl.test';
+#    
+#    my $dbh = DBI->connect($dbSourceName, $userId, $password)
+#                or die "Couldn't connect to database: ". DBI->errstr;
+#    
+#    my $sth = $dbh->prepare_cached('SELECT * FROM People')
+#                    or die "Couldn't prepare statement: ". $dbh->errstr;
+#                    
+#    $sth->execute
+#            or die "Couldn't execute statement: ". $sth->errstr;
+#    my @personData;
+#    
+#    while( @personData = $sth->fetchrow_array()){
+#        print "\t$personData[0]: $personData[1] $personData[2] $personData[3]\n"; 
+#    }
+#    
+#    $sth->finish;
+#    print "\n";
+#    
+#    $dbh->disconnect; 
+#}
 
-print getEmails('username' => $username,
-          'password' => $password,
-          'mailhost' => $mailhost,
-          'port' => $port);
 
-dbManipulation;
+my $configFile = "config.ini";
+tie my %iniConfig, 'Config::IniFiles', (-file => $configFile);
+
+my %mailConfig = %{ $iniConfig{'MailHostConfig'} };
+
+print getEmails('username' => $mailConfig{username},
+          'password' => $mailConfig{password},
+          'mailhost' => $mailConfig{mailhost},
+          'port' => $mailConfig{port} );
+
+
+#dbManipulation;
 
 
 
